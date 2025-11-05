adfd8a5e05a4cfedc81edad41b143a437de18c83
shir0her0 <kumarshireesh304@icloud.com>
2025-11-06 01:56:08 +0530

    Change API_URL to new endpoint
    Updated API_URL to point to the new endpoint.

--- core/transport.js
+++ core/transport.js
@@ -1,5 +1,5 @@
 // core/transport.js
-const API_URL = "https://YOUR_API_ENDPOINT.example.com/collect"; // << REPLACE ME
+const API_URL = "https://5ucetmzkr2.execute-api.us-east-1.amazonaws.com/"; // << REPLACE ME
 
 export class Transport {
   constructor() {
